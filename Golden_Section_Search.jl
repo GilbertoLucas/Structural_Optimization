74e054f5a3a503ca45bf22de315a9c25de2e970e
Gilberto Lucas <31942165+GilbertoLucas@users.noreply.github.com>
2022-01-20 11:56:23 -0300

    add Golden section search with bracket minimum

---
+++ Golden_Section_Search.jl
@@ -0,0 +1,60 @@
+using Plots
+
+using Plots
+
+function bracket_minimum(f, x=0; s=1e-2, k=2.0)
+    a, ya = x, f(x)
+    b, yb = a + s, f(a + s)
+    if yb > ya
+        a, b = b, a
+        ya, yb = yb, ya
+        s = -s
+    end
+    while true
+        c, yc = b + s, f(b + s)
+        if yc > yb
+            return a < c ? (a, c) : (c, a)
+        end
+        a, ya, b, yb = b, yb, c, yc
+        s *= k
+    end
+end
+
+
+φ = MathConstants.φ
+function golden_section_search(f, a, b, n)
+    ρ = φ-1
+    d = ρ * b + (1 - ρ)*a
+    yd = f(d)
+    for i = 1 : n-1
+        c = ρ*a + (1 - ρ)*b
+        yc = f(c)
+        if yc < yd
+            b, d, yd = d, c, yc
+        else
+            a, b = b, c
+        end
+    end
+    return a < b ? (a, b) : (b, a)
+end
+    
+
+
+f = x->sin(10x)+cos(3x)
+x0 = 3.4
+#busca do intervalo
+a,c = bracket_minimum(f,x0)
+
+#numero de iteracoes
+n=(c-a)/(10^-3*log(φ))
+
+am,bm = golden_section_search(f,a,c,n)
+(am,f(am))
+am,bm
+
+
+plot(f,0.,4.,lw=2,draw_arrow="true")
+scatter!([a,c],[f(a),f(c)])
+
+plot(f,0.,4.,lw=2,draw_arrow="true")
+scatter!([am,bm],[ff(am),f(bm)])
